e682a2e0bc62f3da48ebbfec9a86b717366f5c18
[dyad] <git@dyad.sh>
2025-09-04 13:25:48 +0530

    [dyad] Fixed editor and printing bugs - wrote 2 file(s)

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -375,7 +375,7 @@ const EditorPage = () => {
         <UpgradeModal />
       </div>
       <div id="print-container">
-        {dataToPrint && <PrintPreview script={dataToPrint.script} titlePage={dataToTPrint} />}
+        {dataToPrint && <PrintPreview script={dataToPrint.script} titlePage={dataToPrint.titlePage} />}
       </div>
     </>
   );
