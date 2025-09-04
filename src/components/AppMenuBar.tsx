5569c84a6aa4d5da873683ff97daa454f74bc26b
[dyad] <git@dyad.sh>
2025-09-04 16:06:16 +0530

    [dyad] Expanded the application's menu bar - wrote 1 file(s)

--- src/components/AppMenuBar.tsx
+++ src/components/AppMenuBar.tsx
@@ -97,14 +97,20 @@ export const AppMenuBar = ({ onFindClick, onPrint, scriptContent, titlePageConte
       <MenubarMenu>
         <MenubarTrigger>Edit</MenubarTrigger>
         <MenubarContent>
-          <MenubarItem onClick={() => showPlaceholder('Undo')}>Undo</MenubarItem>
-          <MenubarItem onClick={() => showPlaceholder('Redo')}>Redo</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Undo')}>Undo <MenubarShortcut>Ctrl+Z</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Redo')}>Redo <MenubarShortcut>Ctrl+Y</MenubarShortcut></MenubarItem>
           <MenubarSeparator />
-          <MenubarItem onClick={() => showPlaceholder('Cut')}>Cut</MenubarItem>
-          <MenubarItem onClick={() => showPlaceholder('Copy')}>Copy</MenubarItem>
-          <MenubarItem onClick={() => showPlaceholder('Paste')}>Paste</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Cut')}>Cut <MenubarShortcut>Ctrl+X</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Copy')}>Copy <MenubarShortcut>Ctrl+C</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Paste')}>Paste <MenubarShortcut>Ctrl+V</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Delete')}>Delete</MenubarItem>
           <MenubarSeparator />
-          <MenubarItem onClick={onFindClick}>Find & Replace... <MenubarShortcut>⌘F</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Select All')}>Select All <MenubarShortcut>Ctrl+A</MenubarShortcut></MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={onFindClick}>Find & Replace... <MenubarShortcut>Ctrl+F</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Find Next')}>Find Next <MenubarShortcut>Ctrl+G</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Find Previous')}>Find Previous <MenubarShortcut>Ctrl+Shift+G</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Go To')}>Go To... <MenubarShortcut>Ctrl+T</MenubarShortcut></MenubarItem>
         </MenubarContent>
       </MenubarMenu>
 
@@ -130,12 +136,20 @@ export const AppMenuBar = ({ onFindClick, onPrint, scriptContent, titlePageConte
       <MenubarMenu>
         <MenubarTrigger>Format</MenubarTrigger>
         <MenubarContent>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Scene Heading</MenubarItem>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Action</MenubarItem>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Character</MenubarItem>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Dialogue</MenubarItem>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Parenthetical</MenubarItem>
-          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Transition</MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Scene Heading <MenubarShortcut>Ctrl+1</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Action <MenubarShortcut>Ctrl+2</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Character <MenubarShortcut>Ctrl+3</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Dialogue <MenubarShortcut>Ctrl+4</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Parenthetical <MenubarShortcut>Ctrl+5</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showSuccess('Use the format toolbar in the editor view.')}>Transition <MenubarShortcut>Ctrl+6</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Shot')}>Shot <MenubarShortcut>Ctrl+7</MenubarShortcut></MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => showPlaceholder('Bold')}>Bold <MenubarShortcut>Ctrl+B</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Italic')}>Italic <MenubarShortcut>Ctrl+I</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Underline')}>Underline <MenubarShortcut>Ctrl+U</MenubarShortcut></MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => showPlaceholder('Upper/Lower Case')}>Upper/Lower Case <MenubarShortcut>Ctrl+K</MenubarShortcut></MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Make Dual Dialogue')}>Make Dual Dialogue <MenubarShortcut>Ctrl+D</MenubarShortcut></MenubarItem>
         </MenubarContent>
       </MenubarMenu>
 
@@ -143,9 +157,14 @@ export const AppMenuBar = ({ onFindClick, onPrint, scriptContent, titlePageConte
         <MenubarTrigger>Document</MenubarTrigger>
         <MenubarContent>
           <MenubarItem onClick={() => onViewChange('titlepage')}>Title Page</MenubarItem>
-          <MenubarItem onClick={() => onViewChange('logline')}>Logline</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Index Cards')}>Index Cards</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Page Layout')}>Page Layout</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Header and Footer')}>Header and Footer</MenubarItem>
+          <MenubarSeparator />
           <MenubarItem onClick={() => onViewChange('synopsis')}>Synopsis</MenubarItem>
           <MenubarItem onClick={() => onViewChange('notes')}>Notes</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => showPlaceholder('Check Spelling')}>Check Spelling <MenubarShortcut>Ctrl+Alt+K</MenubarShortcut></MenubarItem>
         </MenubarContent>
       </MenubarMenu>
       
@@ -177,9 +196,22 @@ export const AppMenuBar = ({ onFindClick, onPrint, scriptContent, titlePageConte
       <MenubarMenu>
         <MenubarTrigger>Production</MenubarTrigger>
         <MenubarContent>
-          <MenubarItem onClick={() => showPlaceholder('Scene Breakdown')}>Scene Breakdown</MenubarItem>
-          <MenubarItem onClick={() => showPlaceholder('Character Reports')}>Character Reports</MenubarItem>
-          <MenubarItem onClick={() => showPlaceholder('Location Reports')}>Location Reports</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Page Numbering')}>Page Numbering</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Scene Numbering')}>Scene Numbering</MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Revisions</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem onClick={() => showPlaceholder('Mark Revision')}>Mark Revision</MenubarItem>
+              <MenubarItem onClick={() => showPlaceholder('Next Revision')}>Next Revision</MenubarItem>
+              <MenubarItem onClick={() => showPlaceholder('Previous Revision')}>Previous Revision</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => showPlaceholder('Omit Scene')}>Omit Scene</MenubarItem>
+          <MenubarItem onClick={() => showPlaceholder('Scene Versions')}>Scene Versions</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => showPlaceholder('Reports')}>Reports</MenubarItem>
         </MenubarContent>
       </MenubarMenu>
 
